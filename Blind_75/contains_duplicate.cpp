5b84e6d695d63472b908991b4917454868b915eb
anisal-monarch <anisal@wpi.edu>
2023-11-11 16:46:02 -0800

    blind 75

---
+++ Blind_75/contains_duplicate.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        // bool a=true;
+        std::sort(nums.begin(), nums.end());
+        for (int i=0; i<nums.size()-1; i++){
+            if (nums[i]==nums[i+1]){
+                return true;
+            }
+            }
+        
+        return false;
+
+    }
+};
+
+int main(){
+    vector<int> g = {1, 2, 3, 4, 5};
+    vector<int> p = {1, 2, 3, 1, 2};
+    Solution S;
+    std::cout<<S.containsDuplicate(g)<<std::endl;
+    std::cout<<S.containsDuplicate(p)<<std::endl;
+}
